607a54d3ff57837f4675f2d58afdab7bcd716a6a
Daniel Lowe <dnllowe@yahoo.com>
2016-08-24 02:36:28 -0400

    Revert "Updating Android to Hide Nav"
    This reverts commit 345b40c0f2f709f5130b868f8b5c13e089ecc8f3.

--- cocos2d/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos2d/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -37,7 +37,6 @@ import android.os.Message;
 import android.preference.PreferenceManager.OnActivityResultListener;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -70,17 +69,6 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
     private Cocos2dxEditBoxHelper mEditBoxHelper = null;
     private boolean hasFocus = false;
 
-    public void hideSystemUI(){
-    	getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    	return;
- 	}
-    
     public Cocos2dxGLSurfaceView getGLSurfaceView(){
         return  mGLSurfaceView;
     }
@@ -269,7 +257,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        hideSystemUI();
+
         onLoadNativeLibraries();
         SDKBox.init(this);
 
@@ -338,7 +326,6 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
     
     private void resumeIfHasFocus() {
         if(hasFocus) {
-        	hideSystemUI();
         	Cocos2dxHelper.onResume();
         	mGLSurfaceView.onResume();
         }
